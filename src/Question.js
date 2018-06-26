6437a693b195b6381223b3e901f9db0dd38e76f1
Aditya Kuppili <akuppili45@gmail.com>
2018-06-26 15:54:54 -0700

    made next button switch questions

--- src/Question.js
+++ src/Question.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Choices from './Choices';
+import shuffle from 'shuffle-array';
 class Question extends Component {
   constructor(props){
     super(props);
-    this.state = {selectedOption: '', numberCorrect: 0, questionSet: [], countryChoices: [], targetCountry: {}};
+    this.state = {questionSetIndex: 0, selectedOption: '', numberCorrect: 0, questionSet: [], countryChoices: [], targetCountry: {}, possibleAnswerChoices: [], rightAnswer: '', question: ''};
     this.handleRadioChange = this.handleRadioChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -22,22 +23,54 @@ class Question extends Component {
             let finalObj = {country: countryName, capital: capitalName, flag: flagImg};
             return finalObj;
           });
-          //Array of random indexes to point to 4 random integer values
-          let randomIndexes = [];
-          //fill array randomIndexes with 4 random integers that are different
-          while(randomIndexes.length < 4){
-            let randomNumber = Math.floor(Math.random() * countryData.length);
-            if(randomIndexes.indexOf(randomNumber) > -1){
-              continue;
+          countryData = shuffle(countryData);
+          let fullQuestions = Array(10).fill().map((value, index) => {
+            if(index != 0){
+              return {possibleAnswers: [countryData[index*10].capital, countryData[index*10+1].capital, countryData[index*10+2].capital, countryData[index].capital], rightAnswer: countryData[index].capital, quest: countryData[index].country};
             }
-            randomIndexes[randomIndexes.length] = randomNumber;
-          }
-          choices = Array(randomIndexes.length).fill().map((value, index) => {
-            return countryData[randomIndexes[index]];
+            else{
+              return {possibleAnswers: [countryData[index+100].capital, countryData[index+101].capital, countryData[index+102].capital, countryData[index].capital], rightAnswer: countryData[index].capital, quest: countryData[index].country};
+            }
           });
-          let target = choices[Math.floor(Math.random() * choices.length)];
-          let newState = {countryChoices: choices, targetCountry: target};
-          this.setState(newState);
+          let setOfQuestionsObj = {questionSet: fullQuestions, possibleAnswerChoices: shuffle(fullQuestions[this.state.questionSetIndex].possibleAnswers), rightAnswer: fullQuestions[this.state.questionSetIndex].rightAnswer, question: fullQuestions[this.state.questionSetIndex].quest};
+          this.setState(setOfQuestionsObj);
+          console.log(this.state);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          // let question = {choices1: [], quesAns: {}};//later change to array
+          // //Array of random indexes to point to 4 random integer values
+          // let randomIndexes = [];
+          // //fill array randomIndexes with 4 random integers that are different
+          // while(randomIndexes.length < 4){
+          //   let randomNumber = Math.floor(Math.random() * countryData.length);
+          //   if(randomIndexes.indexOf(randomNumber) > -1){
+          //     continue;
+          //   }
+          //   randomIndexes[randomIndexes.length] = randomNumber;
+          // }
+          // choices = Array(randomIndexes.length).fill().map((value, index) => {
+          //   return countryData[randomIndexes[index]];
+          // });
+          // console.log(choices);
+          // let target = choices[Math.floor(Math.random() * choices.length)];
+          // console.log(target);
+          // question = {choices1: choices, quesAns: target};
+          // let newState = {countryChoices: question.choices1, targetCountry: question.quesAns};
+          // this.setState(newState);
+          // console.log(this.state);
       });
   }
   handleRadioChange(e){
@@ -47,32 +80,36 @@ class Question extends Component {
     e.preventDefault();
     // console.log(`You have selected ${this.state.selectedOption}`);
     //this.props.compareValue should be target capital in App.js
-    if(this.state.targetCountry.capital === this.state.selectedOption){
-        this.setState({numberCorrect: this.state.numberCorrect + 1});
-    }
+    // if(this.state.targetCountry.capital === this.state.selectedOption){
+    //     this.setState({numberCorrect: this.state.numberCorrect + 1});
+    // }
+    //this.setState({questionSetIndex: this.state.questionSetIndex + 1, possibleAnswerChoices: this.state.questionSet[this.state.questionSetIndex+1].possibleAnswers, rightAnswer: this.state.questionSet[this.state.questionSetIndex+1].rightAnswer});
+    this.setState({questionSetIndex: this.state.questionSetIndex + 1, possibleAnswerChoices: this.state.questionSet[this.state.questionSetIndex+1].possibleAnswers, rightAnswer: this.state.questionSet[this.state.questionSetIndex+1].rightAnswer, question: this.state.questionSet[this.state.questionSetIndex+1].quest});
+    console.log(this.state);
+
     
   }
   render() {
-    if(this.state.countryChoices.length != 0){
+    if(this.state.possibleAnswerChoices.length != 0){
       return (
         <div className="App">
           <form>
-          <h1>{this.state.targetCountry.country}</h1>
+          <h1>{this.state.question}</h1>
          <label>
-           <input type='radio' value={this.state.countryChoices[0].capital} checked={this.state.selectedOption === this.state.countryChoices[0].capital} onChange={this.handleRadioChange}/>
-           {this.state.countryChoices[0].capital}
+           <input type='radio' value={this.state.possibleAnswerChoices[0]} checked={this.state.selectedOption === this.state.possibleAnswerChoices[0]} onChange={this.handleRadioChange}/>
+           {this.state.possibleAnswerChoices[0]}
          </label>
          <label>
-           <input type='radio' value={this.state.countryChoices[1].capital} checked={this.state.selectedOption === this.state.countryChoices[1].capital} onChange={this.handleRadioChange}/>
-           {this.state.countryChoices[1].capital}
+           <input type='radio' value={this.state.possibleAnswerChoices[1]} checked={this.state.selectedOption === this.state.possibleAnswerChoices[1]} onChange={this.handleRadioChange}/>
+           {this.state.possibleAnswerChoices[1]}
          </label>
          <label>
-           <input type='radio' value={this.state.countryChoices[2].capital} checked={this.state.selectedOption === this.state.countryChoices[2].capital} onChange={this.handleRadioChange}/>
-           {this.state.countryChoices[2].capital}
+           <input type='radio' value={this.state.possibleAnswerChoices[2]} checked={this.state.selectedOption === this.state.possibleAnswerChoices[2]} onChange={this.handleRadioChange}/>
+           {this.state.possibleAnswerChoices[2]}
          </label>
          <label>
-           <input type='radio' value={this.state.countryChoices[3].capital} checked={this.state.selectedOption === this.state.countryChoices[3].capital} onChange={this.handleRadioChange}/>
-           {this.state.countryChoices[3].capital}
+           <input type='radio' value={this.state.possibleAnswerChoices[3]} checked={this.state.selectedOption === this.state.possibleAnswerChoices[3]} onChange={this.handleRadioChange}/>
+           {this.state.possibleAnswerChoices[3]}
          </label>
          <button onClick={this.handleSubmit}>Submit</button>
          <div>{this.state.numberCorrect}</div>
