6d24b8c3be98f916fb43430af03b7077e591dc2e
Aditya Kuppili <akuppili45@gmail.com>
2018-06-26 00:03:05 -0700

    new stuff

--- src/Question.js
+++ src/Question.js
@@ -40,7 +40,6 @@ class Question extends Component {
     if(this.state.countryChoices.length != 0){
       return (
         <div className="App">
-        <h1>New branch</h1>
           <form>
             <Choices firstValue={this.state.countryChoices[0].capital} secondValue={this.state.countryChoices[1].capital} thirdValue={this.state.countryChoices[2].capital} fourthValue={this.state.countryChoices[3].capital} compareValue={this.state.targetCountry.capital} question={this.state.targetCountry.country} />
             {/* <Choices firstValue={this.state.countryChoices[0].country} secondValue={this.state.countryChoices[1].country} thirdValue={this.state.countryChoices[2].country} fourthValue={this.state.countryChoices[3].country} compareValue={this.state.targetCountry.capital}/> */}
