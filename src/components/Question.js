45e95a2cea709499589480f1c9c9f6a71e3d880b
Aditya Kuppili <akuppili45@gmail.com>
2018-07-15 08:52:30 -0700

    changed game mode refactored

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import {GAME_MODE} from '../actions';
 
-
-const Question = ({questions, question}) => {
+const Question = ({questions, question, changeGameMode}) => {
+    changeGameMode(GAME_MODE.COUNTRY_TO_CAPITAL);
     return(
         <h1>{question}</h1>
     );
--- src/containers/QuestionContent.js
+++ src/containers/QuestionContent.js
@@ -1,9 +1,9 @@
 import { connect } from "react-redux";
 import Question from "../components/Question";
-
+import {changeGameMode} from '../actions';
 const mapStateToProps = state => ({questions: state.showData, currentQuestion: state.showCurrentQuestion, question: state.showData[state.showCurrentQuestion].question});
-
+const mapDispatchToProps = dispatch => ({changeGameMode: gameMode => {dispatch(changeGameMode(gameMode))}});
 // const mapDispatchToProps = dispatch => 
 
-export default connect(mapStateToProps)(Question);
+export default connect(mapStateToProps, mapDispatchToProps)(Question);
 
