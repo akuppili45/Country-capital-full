e2ee8c9878cdea78bb121dc27914baf023542ffa
Aditya Kuppili <akuppili45@gmail.com>
2018-07-16 16:36:12 -0700

    attempting to gather picked answers

--- src/forms/forms.container.js
+++ src/forms/forms.container.js
@@ -4,12 +4,10 @@ import FormComponent from './forms.component';
 import { connect } from 'react-redux';
 import {addPoint, goToNextQuestion} from '../actions';
 let FormContainer = props => {
+    // const rightAnswers;
+    // const pickedAnswers = ;
     const submitForm = formValues => {
-        // if(props.answerIsCorrect){
-        //     console.log("the answer is deffinetley correct")
-        // }
-        //props.goNext(props.currentQuestion)
-        console.log('next')
+        console.log(props.pickedAnswers)
     };
     // console.log(props)
     return <FormComponent onSubmit={submitForm} handleSubmit={props.handleSubmit} questions={props.questions}/>;
@@ -17,7 +15,8 @@ let FormContainer = props => {
 };
 const mapStateToProps = state => {
     return {
-        questions: state.showData
+        questions: state.showData,
+        pickedAnswers: state.form !== {} ? Object.values(state.form.myForm.values) : []
     }
 };
 const mapDispatchToProps = dispatch => ({addPoint: score => dispatch(addPoint(score)), goNext: questionNum => dispatch(goToNextQuestion(questionNum)), clear: form => dispatch()});
