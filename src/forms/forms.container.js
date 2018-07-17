87acd122b7e76483cafa0cf2b0ef860ed2fd4474
Aditya Kuppili <akuppili45@gmail.com>
2018-07-16 17:31:27 -0700

    got picked answers and correct answers

--- src/forms/forms.container.js
+++ src/forms/forms.container.js
@@ -7,16 +7,21 @@ let FormContainer = props => {
     // const rightAnswers;
     // const pickedAnswers = ;
     const submitForm = formValues => {
-        console.log(props.pickedAnswers)
+        const { pickedAnswers } = props;
+        const { questions } = props;
+        const correctAnswers = questions.map(content => content.answer);
+        console.log(correctAnswers)
+        console.log(pickedAnswers)
     };
     // console.log(props)
     return <FormComponent onSubmit={submitForm} handleSubmit={props.handleSubmit} questions={props.questions}/>;
     // return <div></div>
 };
 const mapStateToProps = state => {
+    const valuesExists = state.form.hasOwnProperty('myForm') && state.form.myForm.hasOwnProperty('values') && Object.values(state.form.myForm.values).length === 10;
     return {
         questions: state.showData,
-        pickedAnswers: state.form !== {} ? Object.values(state.form.myForm.values) : []
+        pickedAnswers: valuesExists ? Object.values(state.form.myForm.values) : [] //state.form.myForm.values.length also has to equal 10 or else user cannot skip questions
     }
 };
 const mapDispatchToProps = dispatch => ({addPoint: score => dispatch(addPoint(score)), goNext: questionNum => dispatch(goToNextQuestion(questionNum)), clear: form => dispatch()});
