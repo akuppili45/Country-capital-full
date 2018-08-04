8485750134be33ec4db19b76311841cb306b3aaa
Aditya Kuppili <akuppili45@gmail.com>
2018-08-03 20:45:22 -0700

    removed error when submitting the quiz

--- src/components/CountryToCapital.js
+++ src/components/CountryToCapital.js
@@ -30,37 +30,47 @@ class CountryToCapital extends Component{
         if(currentQuestionIndex === 9){
             buttonText = "Submit!";
         }
-        return (
-            <div>
-            <form>
-                <h1>{currentQuestionObj.question}</h1>
-                
-                <label>
-                    <input type='radio' value={currentQuestionObj.allChoices[0]} checked={this.state.selectedOption === currentQuestionObj.allChoices[0]} onChange={this.handleRadioChange}/>
-                        {currentQuestionObj.allChoices[0]}
-                </label>
-
-                <label>
-                    <input type='radio' value={currentQuestionObj.allChoices[1]} checked={this.state.selectedOption === currentQuestionObj.allChoices[1]} onChange={this.handleRadioChange}/>
-                        {currentQuestionObj.allChoices[1]}
-                </label>
-
-                <label>
-                    <input type='radio' value={currentQuestionObj.allChoices[2]} checked={this.state.selectedOption === currentQuestionObj.allChoices[2]} onChange={this.handleRadioChange}/>
-                        {currentQuestionObj.allChoices[2]}
-                </label>
-
-                <label>
-                    <input type='radio' value={currentQuestionObj.allChoices[3]} checked={this.state.selectedOption === currentQuestionObj.allChoices[3]} onChange={this.handleRadioChange}/>
-                        {currentQuestionObj.allChoices[3]}
-                </label>
-
-                <button onClick={this.handleSubmit}>{buttonText}</button>
-                
-            </form>
-            <label>Score: {this.state.numberCorrect}</label>
-            </div>
+        if(currentQuestionIndex < 10){
+            return (
+                <div>
+                <form>
+                    <h1>{currentQuestionObj.question}</h1>
+                    
+                    <label>
+                        <input type='radio' value={currentQuestionObj.allChoices[0]} checked={this.state.selectedOption === currentQuestionObj.allChoices[0]} onChange={this.handleRadioChange}/>
+                            {currentQuestionObj.allChoices[0]}
+                    </label>
+    
+                    <label>
+                        <input type='radio' value={currentQuestionObj.allChoices[1]} checked={this.state.selectedOption === currentQuestionObj.allChoices[1]} onChange={this.handleRadioChange}/>
+                            {currentQuestionObj.allChoices[1]}
+                    </label>
+    
+                    <label>
+                        <input type='radio' value={currentQuestionObj.allChoices[2]} checked={this.state.selectedOption === currentQuestionObj.allChoices[2]} onChange={this.handleRadioChange}/>
+                            {currentQuestionObj.allChoices[2]}
+                    </label>
+    
+                    <label>
+                        <input type='radio' value={currentQuestionObj.allChoices[3]} checked={this.state.selectedOption === currentQuestionObj.allChoices[3]} onChange={this.handleRadioChange}/>
+                            {currentQuestionObj.allChoices[3]}
+                    </label>
+    
+                    <button onClick={this.handleSubmit}>{buttonText}</button>
+                    
+                </form>
+                <label>Score: {this.state.numberCorrect}</label>
+                </div>
+                );
+        }
+        else{
+            return (
+                <div>
+                    <label>Score: {this.state.numberCorrect}</label>
+                </div>
             );
+        }
+
     }
 }
 export default CountryToCapital;
