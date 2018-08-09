a7869f8fd0654c6c7e097a202b370ffb6d16811d
Aditya Kuppili <akuppili45@gmail.com>
2018-08-08 20:02:33 -0700

    nav bar styling

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -10,19 +10,25 @@ class Navbar extends Component {
     render(){
         return (
          
-            <nav style={{margin:"auto"}}>
-                <Link to='/' style={{padding: "15px"}}>Home</Link>
+            <nav style={{margin:"auto"}} className="navbar navbar-expand">
+              <div className="container-fluid">
+                <Link to='/' style={{padding: "15px"}} className="navbar-brand">Home</Link>
                 { !this.props.currentUser.isAuthenticated ?
                     (
-                        <div style={{margin:"auto"}}>
-                            <Link to='/signup' style={{padding: "15px"}}>Sign Up!</Link>
-                            <Link to='/signin' style={{padding: "15px"}}>Sign In!</Link>
-                        </div>
+                        <ul className="navbar navbar-nav navbar-right">
+                            <li><Link to='/signup' style={{padding: "15px"}}>Sign Up!</Link></li>
+                            <li><Link to='/signin' style={{padding: "15px"}}>Sign In!</Link></li>
+                        </ul>
+                        // <div style={{margin:"auto"}}>
+                        //     <Link to='/signup' style={{padding: "15px"}}>Sign Up!</Link>
+                        //     <Link to='/signin' style={{padding: "15px"}}>Sign In!</Link>
+                        // </div>
                     ) : 
                     (
                         <a onClick={this.logout} style={{padding: "15px", height:"50px"}} className="btn btn-outline-danger">Log out</a>
                     )
                 }
+                </div>
             </nav>
 
         );
